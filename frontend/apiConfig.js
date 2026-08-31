63c8879be6646ea664e0a1cab1bade9003a2e103
Abhinav Reddy <your_email@gmail.com>
2026-08-31 13:02:58 +0530

    Connect frontend to production backend

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -1,10 +1,11 @@
 // apiConfig.js
+
 let baseUrl;
 let socketUrl;
 
-if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "your-deployed-URL";
-  socketUrl = "wss://your-deployed-url";
+if (import.meta.env.PROD) {
+  baseUrl = "https://swift-chat-7wvz.onrender.com";
+  socketUrl = "wss://swift-chat-7wvz.onrender.com";
 } else {
   baseUrl = "http://localhost:4000";
   socketUrl = "ws://localhost:4000";
